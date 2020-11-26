7f3c4fd477f78daf45cdc9e636aa128b01585830
Sam Mudick <smudick@gmail.com>
2020-11-26 12:00:48 -0600

    routing and navbar working

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import '../styles/App.scss';
+import firebase from 'firebase/app';
+import { BrowserRouter as Router } from 'react-router-dom';
+import fbConnection from './helpers/connection';
+import Routes from './helpers/Routes';
+import MyNavbar from './components/Navbar';
 
-class App extends React.Component {
+fbConnection();
+
+export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+      <div className='App'>
+        <Router>
+          <MyNavbar />
+          <Routes />
+        </Router>
       </div>
     );
   }
 }
-
-export default App;
