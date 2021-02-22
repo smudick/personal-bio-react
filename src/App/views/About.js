97dd2f7742f35da29d68123dae8e2e2a8e7cfb7d
Sam Mudick <smudick@gmail.com>
2021-02-21 21:54:52 -0600

    fixed icons and added additional ones

--- src/App/views/About.js
+++ src/App/views/About.js
@@ -1,28 +1,30 @@
 import React from 'react';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Link from '@material-ui/core/Link';
+import IconButton from '@material-ui/core/IconButton';
 
 export default class About extends React.Component {
   render() {
     return (
       <div className='d-flex flex-column align-items-center'>
-        <div className='d-flex justify-content-space-between m-3 about-section'>
-          <div className='d-flex flex-column intro'>
-          <h2>
-            Hello!
-          </h2>
-          <p>
-            I'm Sam, and thanks for visiting my site. I made this site
-            as an easy place to store all of my projects. Please take a moment
-            to have a look around and check out all the things I've been working
-            on. If you see anything you like, or have any questions, feel free
-            to reach out!
-          </p>
-          </div>
+        <div className='d-flex justify-content-lg-around m-3 about-section'>
           <img
             className='pic'
             src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/self-photo.jpeg?alt=media&token=4f887793-09d3-4770-960f-c516a784d2a1'
             alt='sam'
           ></img>
+          <div className='d-flex flex-column intro'>
+            <h2>Hello!</h2>
+            <p>
+              I'm Sam, a full stack developer living in Nashville. Thanks for
+              visiting my site! Please take a moment to have a look around and
+              check out all the things I've been working on. If you see anything
+              you like, or have any questions, feel free to reach out on <a href='https://www.linkedin.com/in/sammudick'target="_blank" rel="noreferrer">LinkedIn</a>, <a href='https://www.github.com/smudick' target="_blank" rel="noreferrer">GitHub</a> or send me an <a href = "mailto: smudick@gmail.com" target="_blank" rel="noreferrer">email</a>!
+            </p>
+          </div>
         </div>
+
         <div className='about'>
           <h4>A little more about me</h4>
           <p>
--- src/App/views/Skills.js
+++ src/App/views/Skills.js
@@ -25,44 +25,65 @@ export default function Skills() {
     <>
       <h2 className='skills-header'>Skills</h2>
       <div className='skills-container'>
-        <Avatar className={classes.large}>
-          <i class='fab fa-js-square'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fab fa-css3-alt'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fab fa-html5'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fab fa-react'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fab fa-sass'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fab fa-github-square'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fab fa-apple'></i>
-        </Avatar>
-        <Avatar className={classes.large}>
-          <i class='fas fa-terminal'></i>
-        </Avatar>
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/js%20icon.png?alt=media&token=1b7853f8-6e54-443e-8b20-a1ecf3667e34'
+          alt='javascript logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/1280px-React-icon.svg.png?alt=media&token=ceb4803a-abd3-4748-a81f-1599edb9e5b1'
+          alt='react logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/C%23.png?alt=media&token=50a2c5f8-5230-4b99-b555-60f2a11749ea'
+          alt='c sharp logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/NET.png?alt=media&token=0d22175c-2735-459e-b4dc-699f4f8b427f'
+          alt='dot net logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/html5-icon.png?alt=media&token=58f8141d-916d-420f-a3fd-84b16a7bf8f3'
+          alt='html logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/css-icon.png?alt=media&token=ae80f524-e3b9-41bb-82d0-857e26c3db70'
+          alt='css logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/sass%20logo.png?alt=media&token=730bcd4d-2010-48a3-aa15-557026ac97c4'
+          alt='sass logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/github%20icon.png?alt=media&token=b5b99c2f-1ed1-4ec8-bcd6-44ed460c50e8'
+          alt='github logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/bootstrap.png?alt=media&token=b13df827-307d-4757-a336-d23f0a58a61b'
+          alt='bootstrap'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/material_ui_logo.jpg?alt=media&token=6c086f35-47bb-480f-8c4a-c03eaf2e8542'
+          alt='material logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/materialize.png?alt=media&token=e15335db-5fe5-41aa-ac45-531cdbd4a898'
+          alt='materialize css'
+        />
         <Avatar
           className={classes.large}
           src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/firebase-logo.png?alt=media&token=6232c6aa-957d-4abd-916c-740c6e1b5e86'
           alt='firebase logo'
-        />
-        <Avatar
-          className={classes.large}
-          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/vscode-logo.png?alt=media&token=39b70d3f-9a81-47f5-82f7-edd7cf33794d'
-          alt='vscode logo'
-        />
-        <Avatar
-          className={classes.large}
-          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/npm.png?alt=media&token=03c9215c-bf44-4018-863f-c088a9cdf57f'
-          alt='npm'
         />
         <Avatar
           className={classes.large}
@@ -76,18 +97,38 @@ export default function Skills() {
         />
         <Avatar
           className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/npm.png?alt=media&token=03c9215c-bf44-4018-863f-c088a9cdf57f'
+          alt='npm'
+        />
+        <Avatar
+          className={classes.large}
           src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/figma.png?alt=media&token=08790ad2-8c13-4586-82a5-42925f790b38'
           alt='figma logo'
         />
         <Avatar
           className={classes.large}
-          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/bootstrap.png?alt=media&token=b13df827-307d-4757-a336-d23f0a58a61b'
-          alt='bootstrap'
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/vscode-logo.png?alt=media&token=39b70d3f-9a81-47f5-82f7-edd7cf33794d'
+          alt='vscode logo'
         />
         <Avatar
           className={classes.large}
-          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/materialize.png?alt=media&token=e15335db-5fe5-41aa-ac45-531cdbd4a898'
-          alt='materialize css'
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/visual%20studio.png?alt=media&token=ffc064e3-f607-4544-b59d-9b960800084b'
+          alt='visual studio logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/apple%20logo.png?alt=media&token=d1f17c38-260b-4115-8296-b458c80bc6fe'
+          alt='apple logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/windows%20logo.png?alt=media&token=3c9dd76b-0e10-45cf-9a4b-166fd05017c7'
+          alt='windows logo'
+        />
+        <Avatar
+          className={classes.large}
+          src='https://firebasestorage.googleapis.com/v0/b/sam-mudick.appspot.com/o/terminal.png?alt=media&token=05ce4a14-ba7e-478a-ac0e-65d279187540'
+          alt='terminal logo'
         />
       </div>
     </>
